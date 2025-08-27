fdbe855ea24eee4f893b55c258ffe0b439e882b8
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-08-27 10:59:10 +0900

    chore: 패키지 설정

---
+++ eslint.config.js
@@ -0,0 +1,61 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+
+/**
+ * https://eslint.org/docs/latest/use/configure/configuration-files-new#glob-patterns
+ */
+export default [
+  {
+    files: ['src/**/*.{js,ts,jsx,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        // ✅ Jest
+        describe: 'readonly',
+        it: 'readonly',
+        expect: 'readonly',
+
+        // ✅ Node
+        require: 'readonly',
+        module: 'readonly',
+
+        // ✅ JSX (for React 17+ automatic runtime)
+        React: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+      import: importPlugin,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      'import/order': ['warn', { alphabetize: { order: 'asc' } }],
+      'padding-line-between-statements': [
+        'error',
+        { blankLine: 'always', prev: 'import', next: '*' },
+        { blankLine: 'any', prev: 'import', next: 'import' },
+      ],
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  prettier,
+];
